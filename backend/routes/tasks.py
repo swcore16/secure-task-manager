6cb0e347c6528d1f9e49858943096e84da04dab4
unknown <Kedumim@CASA.NET>
2026-05-19 18:28:41 +0300

    Add examples to check basic tasks endpoint - and fixed the bug :)

--- backend/routes/tasks.py
+++ backend/routes/tasks.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 tasks_bp = Blueprint("tasks", __name__)
 tasks = [
     {"id": 1, "title": "Learn Flask", "done": False},
-    {"id": 2, "title": "Build project", "done": False}
 ]
 
 @tasks_bp.route("/tasks", methods=["GET"])
 def get_tasks():
     return jsonify(tasks)
+
+@tasks_bp.route("/tasks", methods=["POST"])
+def create_task():
+    data = request.get_json()
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": data.get("title"),
+        "done": False
+    }
+
+    tasks.append(new_task)
+    return jsonify(new_task), 201
