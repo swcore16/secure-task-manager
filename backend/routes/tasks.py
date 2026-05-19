631f54aa66715c07c6f10b92c1d4b728e6bd9fc2
unknown <Kedumim@CASA.NET>
2026-05-19 18:32:50 +0300

    Added the ability to delete a task endpoint

--- backend/routes/tasks.py
+++ backend/routes/tasks.py
@@ -21,3 +21,12 @@ def create_task():
 
     tasks.append(new_task)
     return jsonify(new_task), 201
+
+# deleting an existing endpoint by id:
+@tasks_bp.route("/tasks/<int:task_id>", methods=["DELETE"])
+def delete_task(task_id):
+    global tasks
+
+    tasks = [task for task in tasks if task["id"] != task_id]
+
+    return jsonify({"message": "Task deleted"})
